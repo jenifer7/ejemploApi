f593175814a567c125b5a780c775976ebe9ec524
Jenifer <jeni9jacinto7p@gmail.com>
2019-05-21 00:32:56 -0600

    Se mejoro el archivo js

--- js/script.js
+++ js/script.js
@@ -1,50 +1,59 @@
+var urlV = "https://ghibliapi.herokuapp.com";
+
 $(document).ready(function(){
+
   $.ajax({
-    type:"GET",
+    type: "GET",
     url: "https://ghibliapi.herokuapp.com/people",
     datatype:"json",
 
-    success:function(data){
+      success:function(data){
 
-      var listaP = $('#listado');
-      $.each(data,function(i,item){
-        listaP.append(
-          '<br>'
-          +  '<div>'
-          +     '<p>' + item.name + '</p>'
-          +     '<p>' + item.gender + '</p>'
-          +     '<p>' + item.age + '</p>'
-          +     '<p>' + item.species + '</p>' +
-          '</div>'
-        );
-      });
-      console.log(listaP);
-    },
-    error: function(){
-			console.log("No se ha podido obtener la información");
-		}
+          $.each(data, function(i, item){
+            var one = '<tr>'
+                      +  '<td>' + item.name + '</td>'
+                      +  '<td>' + item.gender + '</td>'
+                      +  '<td>' + item.age + '</td>' +
+                      '</tr>';
+            $("#personas>tbody").append(one);
+          });
+      },
   });
 
   $.ajax({
-    type:"GET",
-    url: "https://ghibliapi.herokuapp.com/films/",
+    type: "GET",
+    url: "https://ghibliapi.herokuapp.com/films",
     datatype:"json",
 
-    success:function(data){
+      success:function(data){
 
-      var films = $('#lista2');
-      $.each(data,function(i,item){
-        films.append(
-          '<br>'
-          +  '<div>'
-          +   '<p>' + item.id + '</p>'
-          +   '<p>' + item.title + '</p>' +
-            '</div>'
-        );
-      });
-    },
-    error: function(){
-			console.log("No se ha podido obtener la información");
-		}
+          $.each(data, function(i, item){
+            var row = '<tr>'
+                      +  '<td>' + item.title + '</td>'
+                      +  '<td>' + item.description + '</td>'
+                      +  '<td>' + item.director + '</td>'
+                      +  '<td>' + item.producer + '</td>' +
+                      '</tr>';
+            $("#filmes>tbody").append(row);
+          });
+      },
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "https://ghibliapi.herokuapp.com/locations",
+    datatype:"json",
+
+      success:function(data){
+
+          $.each(data, function(i, item){
+            var two = '<tr>'
+                      +  '<td>' + item.name + '</td>'
+                      +  '<td>' + item.climate + '</td>'
+                      +  '<td>' + item.terrain + '</td>' +
+                      '</tr>';
+            $("#locat>tbody").append(two);
+          });
+      },
   });
 });
